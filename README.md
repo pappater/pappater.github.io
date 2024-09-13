9d15aae4e5e70a689a2207ac6ed01d0642060c84
pappater <pratheesheswaran@gmail.com>
2024-09-13 13:00:00 +0530

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# pappater.github.io
--- pappater/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-    darkMode: "class", // or 'media' for automatic dark mode based on OS settings
-  },
-  plugins: [],
-};

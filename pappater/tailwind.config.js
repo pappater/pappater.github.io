95612e066009ff6415f65619e632c7a6890480d1
pappater <pratheesheswaran@gmail.com>
2024-09-13 12:50:39 +0530

    Pappater react template added

---
+++ pappater/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    darkMode: "class", // or 'media' for automatic dark mode based on OS settings
+  },
+  plugins: [],
+};
